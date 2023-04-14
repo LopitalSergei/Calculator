81ff2d839a08ae7d8b5a044a1e39a9f982b70de4
Laptev S <sl260401@gmail.com>
2023-04-14 11:11:27 +0300

    v1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,54 @@
 "use strict"
+
+const input = document.getElementById("userInput");
+
+let expression = "";
+
+let stringFlag = false;
+
+let zeroFlag = false;
+
+
+function equal() {
+	if (stringFlag) {
+		expression = expression.substring(0, expression.length - 1)
+	}
+	input.value = eval(expression);
+	expression = "";
+}
+
+function press(num) {
+	if (zeroFlag || input.value == "0") {
+		expression = input.value.substring(0, input.value.length - 1);
+		zeroFlag = false;
+		console.log("if 1: " + expression);
+	}
+
+	if ((num == 0 && stringFlag)) {
+		zeroFlag = true;
+		stringFlag = true;
+
+		console.log("if 2: " + expression);
+	}
+
+	if (typeof (num) == "string") {
+		expression = input.value;
+		console.log("if 3: " + expression);
+		if (stringFlag) {
+			expression = input.value.substring(0, input.value.length - 1);
+			console.log("if 4: " + expression);
+		}
+		stringFlag = true;
+	}
+	else {
+		stringFlag = false;
+		console.log("if 5: " + expression);
+	}
+	expression += num;
+	input.value = expression;
+}
+
+function erese() {
+	input.value = "";
+	expression = "";
+}
