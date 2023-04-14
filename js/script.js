afeefb33f1d1fa439139da09c26103afbc0a4226
Laptev S <sl260401@gmail.com>
2023-04-14 12:02:32 +0300

    v1.1

--- js/script.js
+++ js/script.js
@@ -8,6 +8,8 @@ let stringFlag = false;
 
 let zeroFlag = false;
 
+let signFlag = false;
+
 
 function equal() {
 	if (stringFlag) {
@@ -18,31 +20,39 @@ function equal() {
 }
 
 function press(num) {
+	if (signFlag) {
+		expression = input.value.substring(1, input.value.length);
+
+		console.log(expression);
+		signFlag = false;
+	}
+
+	if (input.value == 0 && typeof (num) == "string" && num != "+" && num != "-") {
+		signFlag = true;
+		console.log(signFlag);
+	}
+
+
 	if (zeroFlag || input.value == "0") {
 		expression = input.value.substring(0, input.value.length - 1);
 		zeroFlag = false;
-		console.log("if 1: " + expression);
 	}
 
 	if ((num == 0 && stringFlag)) {
 		zeroFlag = true;
 		stringFlag = true;
 
-		console.log("if 2: " + expression);
 	}
 
 	if (typeof (num) == "string") {
 		expression = input.value;
-		console.log("if 3: " + expression);
 		if (stringFlag) {
 			expression = input.value.substring(0, input.value.length - 1);
-			console.log("if 4: " + expression);
 		}
 		stringFlag = true;
 	}
 	else {
 		stringFlag = false;
-		console.log("if 5: " + expression);
 	}
 	expression += num;
 	input.value = expression;
